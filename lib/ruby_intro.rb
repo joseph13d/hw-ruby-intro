e97508f75a9f328d2ff6de90d744cf5b57732f97
Josephmac <rjc11a@acu.edu>
2020-01-30 22:50:38 -0600

    first

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,14 +4,45 @@
 
 def sum arr
   # YOUR CODE HERE
+  sum = 0
+  arr.each { |elem| sum += elem }
+  return sum
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  a=nil
+  b=nil
+  for x in arr
+  	if a.nil?
+  		a=x
+  	elsif b.nil?
+  		b=x
+  	elsif x>a
+  		b=a
+  		a=x
+  	elsif x>b
+  		b=x
+  	end
+  end
+  if b
+  	return a+b
+  elsif a
+  	return a
+  else
+  	return 0
+  end
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  b = arr.combination(2)
+  for x in b
+  	if x.sum == n
+  		return true
+  	end
+  end
+  return false
 end
 
 # Part 2
